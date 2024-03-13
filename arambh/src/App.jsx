40f9c68b87c400ec52b12d63eb977cbfb35549fc
MahimaSBisen <mahimasbisen@gmail.com>
2024-03-14 03:11:30 +0530

    start project

---
+++ arambh/src/App.jsx
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <></>;
+}
+
+export default App;

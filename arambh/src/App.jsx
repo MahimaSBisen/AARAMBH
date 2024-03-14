e9a979f207924075d7fc3419511a7735d97d8f75
MahimaSBisen <mahimasbisen@gmail.com>
2024-03-14 09:37:14 +0530

    Components folder

--- arambh/src/App.jsx
+++ arambh/src/App.jsx
@@ -1,7 +1,9 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import "./App.css";
+// import "./App.css";
+import "../scss/styles.scss";
+import * as bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header";
 import OD from "./OD";
--- arambh/src/main.jsx
+++ arambh/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // import "./index.css";
+import "../scss/styles.scss";
+import * as bootstrap from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(

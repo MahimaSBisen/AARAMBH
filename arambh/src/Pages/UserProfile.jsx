025549588a8935802be4ef947ed14311cba13234
MahimaSBisen <mahimasbisen@gmail.com>
2024-04-20 08:03:29 +0530

    Notification updated

--- arambh/src/Pages/UserProfile.jsx
+++ arambh/src/Pages/UserProfile.jsx
@@ -1,11 +1,26 @@
 import styles from './UserProfile.module.css'
+import { MdPhotoCamera } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 
 function UserProfile() {
   return (
     <>
     <div className={styles.userDiv}>
-
+        <h1 className={styles.head}>Profile</h1>
+        
+        <div className={styles.menu}>
+        {/* <div className={styles.camera}>  */}
+        <Link><MdPhotoCamera className={styles.cameraIcon}/></Link>
+        {/* </div> */}
+        <h4 className={styles.menuHead}>Your Account</h4>
+        <h4 className={styles.menuHead}>Eligibility</h4>
+        
+        <h4 className={styles.menuHead}>Setting</h4>
+        <hr></hr>
+        <h4 className={styles.menuHead}>Log Out</h4>
+        {/* <h4 className={styles.menuHead}>My Account</h4> */}
+        </div>
     </div>
     </>
   )
